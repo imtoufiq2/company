36a10b45a97eebf0ea23b97ceb4e706d4be85965
Keval Khatri <kevalkhatri32@gmail.com>
2024-06-20 15:34:11 +0530

    add nomination (edit percentage)

--- src/components/pages/add-nomination/index.js
+++ src/components/pages/add-nomination/index.js
@@ -7,59 +7,30 @@ import { getData } from "../../../utils/Crypto";
 import Button from "../../atoms/button";
 import OptionHeading from "../../atoms/optionHeading";
 import OptionHeader from "../../molecules/optionHeader";
-import ShowNominee from "../../organism/ShowNominee";
+import NomineeModal from "./../../organism/nomineeModal/index";
 
 const AddNomination = () => {
   const [nomineeData, setNomineeData] = React.useState([]);
-  const [selectedNomineeData, setSelectedNomineeData] = React.useState([]);
-
-  const getNomineeData = async () => {
-    let xmlData = "";
-    try {
-      const response = await axios.post(
-        "https://altcaseinvestor.we3.in/api/v1/profile",
-        {
-          display_location: "Nomination",
-          method: "Get",
-          investor_id: Number(getData("userData")?.investor_id),
-        },
-      );
-      if (response.status === 200) {
-        setNomineeData(response?.data?.data);
-      }
-
-      // const getnomineelist = response.data.nomineeList || [];
-      // getnomineelist.forEach((nominee) => {
-      //   xmlData += `<R><N_ID>${nominee.nomineeId}</N_ID><N_VALUE>10</N_VALUE></R>`;
-      // });
-
-      // console.log(xmlData);
-    } catch (e) {
-      console.error(e);
-    }
+  const [isModalActive, setIsModalActive] = React.useState(false);
+  const [currentNominee, setCurrentNominee] = React.useState(null);
+  const [totalShare, setTotalShare] = React.useState(0);
+  const initialValues = {
+    fullName: "",
+    Relationship: "",
+    PAN: "",
+    PercentShare: "",
+    DateOfBirth: "",
+    Address: "",
+    sameAsInvestor: false,
+    correspondentAddress: {
+      addressLine1: "",
+      addressLine2: "",
+      pincode: "",
+      city: "",
+      state: "",
+      country: "",
+    },
   };
-
-  const calculateTotalShare = (data) => {
-    return data.reduce((total, nominee) => {
-      return total + nominee.PercentShare;
-    }, 0);
-  };
-
-  const [totalShare, setTotalShare] = React.useState(
-    calculateTotalShare(selectedNomineeData),
-  );
-  React.useEffect(() => {
-    document.body.style.backgroundColor = "#F9FAFB";
-
-    const updatedTotalShare = calculateTotalShare(selectedNomineeData);
-    setTotalShare(updatedTotalShare);
-    getNomineeData();
-    // console.log("nomineedata=>>>>", nomineeData);
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, [selectedNomineeData]);
-
   const validationSchema = Yup.object().shape({
     fullName: Yup.string().required("Full Name is required"),
     Relationship: Yup.string().required("Relationship is required"),
@@ -74,12 +45,11 @@ const AddNomination = () => {
       .test("maxTotalShare", `Total share cannot exceed 100%`, (value) => {
         return value + totalShare <= 100;
       }),
-    DateOfBirth: Yup.string()
-      .required("Date of Birth is required")
-      .matches(
-        /^(0?[1-9]|[12][0-9]|3[01])[-/](0?[1-9]|1[0-2])[-/]\d{4}$/,
-        "Invalid Date of Birth format (DD/MM/YYYY)",
-      ),
+    DateOfBirth: Yup.string().required("Date of Birth is required"),
+    // .matches(
+    //   /^(0?[1-9]|[12][0-9]|3[01])[-/](0?[1-9]|1[0-2])[-/]\d{4}$/,
+    //   "Invalid Date of Birth format (DD/MM/YYYY)",
+    // ),
     correspondentAddress: Yup.object().shape({
       addressLine1: Yup.string().when("$sameAsInvestor", {
         is: false,
@@ -114,40 +84,145 @@ const AddNomination = () => {
     }),
   });
 
-  const initialValues = {
-    fullName: "",
-    Relationship: "",
-    PAN: "",
-    PercentShare: "",
-    DateOfBirth: "",
-    Address: "",
-    sameAsInvestor: false,
-    correspondentAddress: {
-      addressLine1: "",
-      addressLine2: "",
-      pincode: "",
-      city: "",
-      state: "",
-      country: "",
-    },
+  const calculateTotalShare = (nomineeData) => {
+    const totalNomineeShare = nomineeData.reduce((total, nominee) => {
+      return total + Number(nominee.percentage);
+    }, 0);
+    setTotalShare(totalNomineeShare);
+    return totalNomineeShare;
   };
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString(undefined, options);
+  };
+  const getNomineeData = async () => {
+    // let xmlData = "";
+    try {
+      const response = await axios.post(
+        "https://altcaseinvestor.we3.in/api/v1/profile",
+        {
+          display_location: "Nomination",
+          method: "Get",
+          investor_id: Number(getData("userData")?.investor_id),
+        },
+      );
+      const selectedNominee = response.data.data.map((el) => {
+        return { ...el, isSelected: false };
+      });
+      setNomineeData(selectedNominee);
 
+      calculateTotalShare(selectedNominee);
+
+      // const getnomineelist = response.data.nomineeList || [];
+      // getnomineelist.forEach((nominee) => {
+      //   xmlData += `<R><N_ID>${nominee.nomineeId}</N_ID><N_VALUE>10</N_VALUE></R>`;
+      // });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const handleCheckboxChange = (nominee_id) => {
+    const newdt = nomineeData.map((item) => {
+      if (item.nominee_id === nominee_id) {
+        return { ...item, isSelected: !item.isSelected };
+      }
+      return item;
+    });
+    console.log(newdt);
+    setNomineeData((prevState) => {
+      return prevState.map((item) => {
+        if (item.nominee_id === nominee_id) {
+          return { ...item, isSelected: !item.isSelected };
+        }
+        return item;
+      });
+    });
+    // const isSelected = selectedNomineeData.some(
+    //   (data) => data.nominee_id === nominee_id,
+    // );
+    // console.log(isSelected);
+    // if (!isSelected) {
+    //   const selectedNominee = nomineeData.find(
+    //     (nominee) => nominee.nominee_id === nominee_id,
+    //   );
+    //   console.log(selectedNominee);
+    //   setSelectedNomineeData((prevData) => [...prevData, selectedNominee]);
+    // } else {
+    //   setSelectedNomineeData((prevData) =>
+    //     prevData.filter((data) => data.nominee_id !== nominee_id),
+    //   );
+    // }
+  };
+  const handleModalShareChange = (nominee) => {
+    setCurrentNominee(nominee);
+    setIsModalActive(true);
+  };
+  const updateShare = (nominee, newShare) => {
+    setNomineeData((prevData) =>
+      prevData.map((data) =>
+        data.nominee_id === nominee.nominee_id
+          ? { ...data, percentage: newShare }
+          : data,
+      ),
+    );
+    // Calculating Percentage again after nominee percentage is changes
+    const newNominees = nomineeData.map((data) =>
+      data.nominee_id === nominee.nominee_id
+        ? { ...data, percentage: newShare }
+        : data,
+    );
+    const totalNomineeShare = calculateTotalShare(newNominees);
+    if (totalNomineeShare !== 100) {
+      toast.error("Percentage share has to be 100%");
+    }
+    setIsModalActive(false);
+    // Update updatedData to reflect the change
+    // setUpdatedData(
+    // const updatedNomineeData = updatedData.map((nominee) =>
+    //   nominee.nominee_id === currentNominee.nominee_id
+    //     ? { ...nominee, percentage: newShare }
+    //     : nominee,
+    // );
+    // );
+    // console.log("updatedNomineeDataupdatedNomineeData", updatedNomineeData);
+    // console.log(
+    //   "selectedNomineeDataselectedNomineeDataselectedNomineeData",
+    //   selectedNomineeData,
+    // );
+    // Optionally, force a re-render by setting a key on the component
+    // This is just an example; you'll need to adapt it based on where you render your nominees
+    // For example, if you're rendering them inside a parent component, you could add a key prop to that component:
+    // <ParentComponent key={Math.random()} />
+  };
   const handleSaveAndAddMore = (values, { resetForm }) => {
-    const newPercentShare = Number(values.PercentShare);
-    setNomineeData((prevData) => [
-      ...prevData,
-      { ...values, PercentShare: newPercentShare },
-    ]);
+    console.log(values);
+    // const newPercentShare = Number(values.PercentShare);
+    // setNomineeData((prevData) => [
+    //   ...prevData,
+    //   { ...values, PercentShare: newPercentShare },
+    // ]);
 
     // Recalculate totalShare after updating nomineeData
-    const updatedTotalShare = calculateTotalShare([
-      ...selectedNomineeData,
-      { ...values, PercentShare: newPercentShare },
-    ]);
-    setTotalShare(updatedTotalShare);
+    // const updatedTotalShare = calculateTotalShare([
+    //   ...selectedNomineeData,
+    //   { ...values, PercentShare: newPercentShare },
+    // ]);
+    // setTotalShare(updatedTotalShare);
 
     resetForm();
   };
+
+  React.useEffect(() => {
+    document.body.style.backgroundColor = "#F9FAFB";
+    // const updatedTotalShare = calculateTotalShare(selectedNomineeData);
+    // console.log(updatedTotalShare);
+    // setTotalShare(updatedTotalShare);
+    getNomineeData();
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
     <div className="mx-auto mb-8 mt-8 flex w-full max-w-[1008px] flex-col gap-5  px-6 sm:max-w-[592px] md:gap-7">
@@ -157,18 +232,112 @@ const AddNomination = () => {
       />
       {/* Show the registered nominees */}
       <div className="flex flex-col gap-4">
-        <ShowNominee
-          setSelectedNomineeData={setSelectedNomineeData}
-          selectedNomineeData={selectedNomineeData}
-          nomineeData={nomineeData}
-        />
+        {isModalActive && (
+          <NomineeModal
+            setShowLoader={setIsModalActive}
+            showLoader={isModalActive}
+            currentShare={currentNominee?.percentage || 100}
+            updateShare={updateShare}
+            cur={currentNominee}
+          />
+        )}
+        {nomineeData.map((nominee) => (
+          <div
+            key={nominee.nominee_id}
+            className={`flex flex-col gap-5 rounded-xl border-[0.5px] bg-white p-5 md:p-8 ${nominee.isSelected ? "border-green-500" : "border-none"}`}
+          >
+            <div className="flex justify-between">
+              <h4 className="semi-bold-text text-sm leading-6 tracking-[-0.2] text-[#21B546]">
+                Nominee
+              </h4>
+              <div className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="bg-green-500"
+                  checked={nominee.isSelected}
+                  onChange={() => handleCheckboxChange(nominee.nominee_id)}
+                />
+              </div>
+            </div>
+
+            <div className="-mt-5">
+              <p className="regular-text text-xs leading-5 tracking-[-0.2] text-[#5E718D]">
+                Name
+              </p>
+              <h5 className="medium-text text-sm leading-6 tracking-[-0.2] text-[#1B1B1B]">
+                {nominee.full_name}
+              </h5>
+            </div>
+
+            <div className="grid grid-cols-2">
+              <div>
+                <p className="regular-text text-xs leading-5 tracking-[-0.2] text-[#5E718D]">
+                  Relationship
+                </p>
+                <h5 className="medium-text text-sm leading-6 tracking-[-0.2] text-[#1B1B1B]">
+                  {nominee.relationship}
+                </h5>
+              </div>
+              <div>
+                <p className="regular-text text-xs leading-5 tracking-[-0.2] text-[#5E718D]">
+                  PAN
+                </p>
+                <h5 className="medium-text text-sm leading-6 tracking-[-0.2] text-[#1B1B1B]">
+                  {nominee.pan}
+                </h5>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-2">
+              <div>
+                <p className="regular-text text-xs leading-5 tracking-[-0.2] text-[#5E718D]">
+                  Date of birth
+                </p>
+                <h5 className="medium-text text-sm leading-6 tracking-[-0.2] text-[#1B1B1B]">
+                  {formatDate(nominee.date_of_birth)}
+                </h5>
+              </div>
+              <div>
+                <p className="regular-text text-xs leading-5 tracking-[-0.2] text-[#5E718D]">
+                  Percent Share
+                </p>
+                <div className="flex items-center gap-2">
+                  <h5 className="medium-text text-sm leading-6 tracking-[-0.2] text-[#1B1B1B]">
+                    {nominee.percentage}%
+                  </h5>
+                  <img
+                    src="/images/edit-pencil.svg"
+                    alt="pencil"
+                    className={`min-h-[1.125rem] min-w-[1.125rem] max-w-[38px] ${nominee.isSelected ? "cursor-pointer" : "cursor-default"} rounded-md border px-2 py-[0.2rem] transition-all duration-200 ease-in-out active:scale-95`}
+                    onClick={() => {
+                      if (!nominee.isSelected) {
+                        return;
+                      }
+                      handleModalShareChange(nominee);
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <p className="regular-text text-xs leading-5 tracking-[-0.2] text-[#5E718D]">
+                Address
+              </p>
+              <h5 className="medium-text text-sm leading-6 tracking-[-0.2] text-[#1B1B1B]">
+                {nominee.address_line_1 + ", " + nominee.address_line_2}
+              </h5>
+            </div>
+          </div>
+        ))}
       </div>
       {/* Nominee form */}
-      {totalShare < 100 && (
+      {/* {totalShare < 100 || */}
+      {!nomineeData.some((item) => item.isSelected) && (
         <Formik
+          enableReinitialize
           initialValues={initialValues}
           validationSchema={validationSchema}
-          enableReinitialize
           onSubmit={handleSaveAndAddMore}
         >
           {({ values }) => (
@@ -389,6 +558,7 @@ const AddNomination = () => {
         <Button
           label="Save & Continue"
           type="button"
+          disabled={!(totalShare === 100)}
           className={`medium-text max-h-12  text-base leading-7 tracking-[-0.3]  ${
             totalShare === 100
               ? "bg-[#21B546] text-white"
@@ -409,3 +579,22 @@ const AddNomination = () => {
 };
 
 export default AddNomination;
+
+// const [updatedData, setUpdatedData] = React.useState([]);
+// ${
+//   selectedNomineeData.some(
+//     (data) => data.nominee_id === nominee.nominee_id,
+//   )
+//     ? "border-green-500"
+//     : "border-none"
+// }
+// const [totalShare, setTotalShare] = React.useState(
+//   calculateTotalShare(selectedNomineeData),
+// );
+// const getPercentageShare = (nominee) => {
+//   // console.log("nomineenomineenominee", nominee);
+//   // console.log("nomineeDatanomineeDatanomineeData", nomineeData);
+//   const nd = nomineeData.filter((data) => data.pan === nominee.pan);
+//   // console.log("ndndndnd", nd);
+//   return nd.percent || 100;
+// };
